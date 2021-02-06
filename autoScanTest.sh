f88964affd258dab3fa5e8984aa0ccffb019736a
bkitor <bkitor@gmail.com>
2021-02-06 13:31:14 -0500

    scanner testing

---
+++ autoScanTest.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+cd parser/test/
+
+echo -e "\n****Keyword test****"
+./run_tests.sh k | grep "pIdentifier"
+
+# echo -e "\n****Character test****"
+# ./run_tests.sh c | grep "\." | grep -v "Line"
+
+echo -e "\n****Comment test****"
+./run_tests.sh m | grep -E "@|\.|%" | grep -v "pNewLine" 
+
+echo -e "\n****String Litteral test****"
+./run_tests.sh s | grep -E "@|%|\.|#" | grep -v "pNewLine"
