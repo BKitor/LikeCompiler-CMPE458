5de0d4c0e0b83baf4ef697a7f2354ab8150dee18
Eric Leask <16efl@linux2.caslab.queensu.ca>
2021-02-06 20:01:24 -0500

    double character tokens and testing

--- autoScanTest.sh
+++ autoScanTest.sh
@@ -5,8 +5,11 @@ cd parser/test/
 echo -e "\n****Keyword test****"
 ./run_tests.sh k | grep "pIdentifier"
 
-# echo -e "\n****Character test****"
-# ./run_tests.sh c | grep "\." | grep -v "Line"
+ echo -e "\n****Character test****"
+ ./run_tests.sh c | grep "\." | grep -v "Line"
+ 
+ echo -e "\n****Double Character Tokens test****"
+ ./run_tests.sh d | grep "\." | grep -v "Line"
 
 echo -e "\n****Comment test****"
 ./run_tests.sh m | grep -E "@|\.|%" | grep -v "pNewLine" 
