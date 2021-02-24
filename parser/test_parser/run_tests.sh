b448a41a6a72e0ced3473d3493f3d19956e61a6f
bkitor <bkitor@gmail.com>
2021-02-23 19:17:41 -0500

    fun and public

--- parser/test_parser/run_tests.sh
+++ parser/test_parser/run_tests.sh
@@ -9,19 +9,23 @@ LIKE_LIB=../../lib/pt
 SCANPKG="ptc -o2 -t2 -L $LIKE_LIB packages.pt"
 SCANVAL="ptc -o2 -t2 -L $LIKE_LIB constant_dec.pt"
 SCANVAR="ptc -o2 -t2 -L $LIKE_LIB variable_dec.pt"
+SCANFUN="ptc -o2 -t2 -L $LIKE_LIB function_dec.pt"
 
 pkg_trace(){ ssltrace "$SCANPKG" $LIKE_LIB/parser.def -e; }
 var_trace(){ ssltrace "$SCANVAR" $LIKE_LIB/parser.def -e; }
 val_trace(){ ssltrace "$SCANVAL" $LIKE_LIB/parser.def -e; }
+fun_trace(){ ssltrace "$SCANFUN" $LIKE_LIB/parser.def -e; }
 
 pkg(){ ptc -o1 -L $LIKE_LIB packages.pt; }
-var(){ ptc -o1 -L $LIKE_LIB packages.pt; }
-val(){ ptc -o1 -L $LIKE_LIB packages.pt; }
+var(){ ptc -o1 -L $LIKE_LIB constant_dec.pt; }
+val(){ ptc -o1 -L $LIKE_LIB variable_dec.pt; }
+fun(){ ptc -o1 -L $LIKE_LIB function_dec.pt; }
 
 if [ $# -ne 0 ]; then
     if [[ "$1" == *"p"* ]];then pkg_trace; fi
     if [[ "$1" == *"r"* ]];then var_trace; fi
     if [[ "$1" == *"l"* ]];then val_trace; fi
+    if [[ "$1" == *"f"* ]];then fun_trace; fi
 else
     echo "-- test pkg ---"
     pkg
@@ -29,4 +33,6 @@ else
     var
     echo "-- test val ---"
     val
+    echo "-- test fun ---"
+    fun
 fi
