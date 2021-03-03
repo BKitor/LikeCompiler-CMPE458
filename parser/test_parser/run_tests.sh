a6cf67a255c02be9d5cd713093db1cef349220cb
bkitor <bkitor@gmail.com>
2021-03-02 20:31:20 -0500

    double assigments fully implimented, needs docs

--- parser/test_parser/run_tests.sh
+++ parser/test_parser/run_tests.sh
@@ -14,17 +14,17 @@ SCANCHS="ptc -o2 -t2 -L $LIKE_LIB choose.pt"
 SCANWHI="ptc -o2 -t2 -L $LIKE_LIB while.pt"
 SCANREP="ptc -o2 -t2 -L $LIKE_LIB repeat.pt"
 SCANIF="ptc -o2 -t2 -L $LIKE_LIB elseif.pt"
-SCANDOUB="ptc -o2 -t2 L $LIKE_LIB doubles.pt"
+SCANDOUB="ptc -o2 -t2 -L $LIKE_LIB doubles.pt"
 
 pkg_trace(){ ssltrace "$SCANPKG" $LIKE_LIB/parser.def -e; }
 var_trace(){ ssltrace "$SCANVAR" $LIKE_LIB/parser.def -e; }
 val_trace(){ ssltrace "$SCANVAL" $LIKE_LIB/parser.def -e; }
 fun_trace(){ ssltrace "$SCANFUN" $LIKE_LIB/parser.def -e; }
 chs_trace(){ ssltrace "$SCANCHS" $LIKE_LIB/parser.def -e; }
-eif_trace(){ ssltrace "$SCANCHS" $LIKE_LIB/parser.def -e; }
-whi_trace(){ ssltrace "$SCANIF" $LIKE_LIB/parser.def -e; }
-rep_trace(){ ssltrace "$SCANIF" $LIKE_LIB/parser.def -e; }
-doub_trace(){ ssltrace "$SCANCHS" $LIKE_LIB/parser.def -e; }
+eif_trace(){ ssltrace "$SCANIF" $LIKE_LIB/parser.def -e; }
+whi_trace(){ ssltrace "$SCANWHI" $LIKE_LIB/parser.def -e; }
+rep_trace(){ ssltrace "$SCANREP" $LIKE_LIB/parser.def -e; }
+doub_trace(){ ssltrace "$SCANDOUB" $LIKE_LIB/parser.def -e; }
 
 
 pkg(){ ptc -o1 -L $LIKE_LIB packages.pt; }
@@ -44,7 +44,6 @@ if [ $# -ne 0 ]; then
     if [[ "$1" == *"f"* ]];then fun_trace; fi
     if [[ "$1" == *"c"* ]];then chs_trace; fi
     if [[ "$1" == *"d"* ]];then doub_trace; fi
-
     if [[ "$1" == *"w"* ]];then whi_trace; fi
     if [[ "$1" == *"e"* ]];then rep_trace; fi
     if [[ "$1" == *"i"* ]];then eif_trace; fi
