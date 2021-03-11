c903de811acb6d2255d1561ff03e4e92daa9b990
bkitor <bkitor@gmail.com>
2021-03-11 18:07:09 -0500

    WIP

---
+++ semantic/tests/run_parser.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd ..
+make 
+cd tests
+
+LIKE_LIB=../../lib/pt
+
+PARBLK="ptc -o2 -t2 -L $LIKE_LIB block.pt"
+
+blk_trace(){ ssltrace "$PARBLK" $LIKE_LIB/parser.def -e; }
+
+blk(){ ptc -o2 -L $LIKE_LIB packages.pt; }
+
+if [ $# -ne 0 ]; then
+    if [[ "$1" == *"b"* ]];then blk_trace; fi
+else
+    echo "-- test pkg ---"
+    pkg
+fi
--- semantic/tests/run_tests.sh
+++ semantic/tests/run_tests.sh
@@ -6,13 +6,17 @@ cd tests
 
 LIKE_LIB=../../lib/pt
 
-SEMBLK="ptc -o3 -t3 -L $LIKE_LIB blank.pt"
+SEMNUL="ptc -o3 -t3 -L $LIKE_LIB null.pt"
+SEMBLK="ptc -o3 -t3 -L $LIKE_LIB block.pt"
 
-blk_trace(){ ssltrace "$SEMBLK" $LIKE_LIB/semantic.def -e; }
+# nul_trace(){ ssltrace "$SEMNUL" $LIKE_LIB/semantic.def | egrep " *[\.o%]"; }
+nul_trace(){ ssltrace "$SEMNUL" $LIKE_LIB/semantic.def -e ; }
+blk_trace(){ ssltrace "$SEMBLK" $LIKE_LIB/semantic.def -e ; }
 
 blk(){ ptc -o3 -L $LIKE_LIB stringType.pt; }
 
 if [ $# -ne 0 ]; then
+    if [[ "$1" == *"n"* ]];then nul_trace; fi
     if [[ "$1" == *"b"* ]];then blk_trace; fi
 
 else
