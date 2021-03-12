dfae7e8aa9dbd785087f35f73ca0990d6e8dbb1d
bkitor <bkitor@gmail.com>
2021-03-12 16:01:06 -0500

    Like statements and var params

--- semantic/tests/run_parser.sh
+++ semantic/tests/run_parser.sh
@@ -7,14 +7,20 @@ cd ../semantic/tests
 LIKE_LIB=../../lib/pt
 
 PARBLK="ptc -o2 -t2 -L $LIKE_LIB block.pt"
+PARVAR="ptc -o2 -t2 -L $LIKE_LIB variables.pt"
 
 blk_trace(){ ssltrace "$PARBLK" $LIKE_LIB/parser.def -e; }
+var_trace(){ ssltrace "$PARVAR" $LIKE_LIB/parser.def -e; }
 
 blk(){ ptc -o2 -L $LIKE_LIB block.pt; }
+var(){ ptc -o2 -L $LIKE_LIB variables.pt; }
 
 if [ $# -ne 0 ]; then
     if [[ "$1" == *"b"* ]];then blk_trace; fi
+    if [[ "$1" == *"v"* ]];then var_trace; fi
 else
-    echo "-- test blok ---"
+    echo "-- parse block ---"
     blk
+    echo "-- parse variables ---"
+    var
 fi
--- semantic/tests/run_tests.sh
+++ semantic/tests/run_tests.sh
@@ -8,18 +8,24 @@ LIKE_LIB=../../lib/pt
 
 SEMNUL="ptc -o3 -t3 -L $LIKE_LIB null.pt"
 SEMBLK="ptc -o3 -t3 -L $LIKE_LIB block.pt"
+SEMVAR="ptc -o3 -t3 -L $LIKE_LIB variables.pt"
+
 
 # nul_trace(){ ssltrace "$SEMNUL" $LIKE_LIB/semantic.def | egrep " *[\.o%]"; }
 nul_trace(){ ssltrace "$SEMNUL" $LIKE_LIB/semantic.def -e ; }
 blk_trace(){ ssltrace "$SEMBLK" $LIKE_LIB/semantic.def -e ; }
+var_trace(){ ssltrace "$SEMVAR" $LIKE_LIB/semantic.def -e ; }
 
 blk(){ ptc -o3 -L $LIKE_LIB block.pt; }
+var(){ ptc -o3 -L $LIKE_LIB variables.pt; }
 
 if [ $# -ne 0 ]; then
     if [[ "$1" == *"n"* ]];then nul_trace; fi
     if [[ "$1" == *"b"* ]];then blk_trace; fi
-
+    if [[ "$1" == *"v"* ]];then var_trace; fi
 else
-    echo "-- test blk ---"
+    echo "-- semantic block ---"
     blk
+    echo "-- semantic variables ---"
+    var
 fi
