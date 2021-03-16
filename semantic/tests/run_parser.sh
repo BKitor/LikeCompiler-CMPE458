4d02bc12c3e9b845ec6792321c591d90629d8afd
bkitor <bkitor@gmail.com>
2021-03-16 00:03:19 -0400

    Packages should be gucci, fuck pt

--- semantic/tests/run_parser.sh
+++ semantic/tests/run_parser.sh
@@ -8,19 +8,25 @@ LIKE_LIB=../../lib/pt
 
 PARBLK="ptc -o2 -t2 -L $LIKE_LIB block.pt"
 PARVAR="ptc -o2 -t2 -L $LIKE_LIB variables.pt"
+PARPKG="ptc -o2 -t2 -L $LIKE_LIB pkg.pt"
 
 blk_trace(){ ssltrace "$PARBLK" $LIKE_LIB/parser.def -e; }
 var_trace(){ ssltrace "$PARVAR" $LIKE_LIB/parser.def -e; }
+pkg_trace(){ ssltrace "$PARPKG" $LIKE_LIB/parser.def -e; }
 
 blk(){ ptc -o2 -L $LIKE_LIB block.pt; }
 var(){ ptc -o2 -L $LIKE_LIB variables.pt; }
+var(){ ptc -o2 -L $LIKE_LIB pkg.pt; }
 
 if [ $# -ne 0 ]; then
     if [[ "$1" == *"b"* ]];then blk_trace; fi
     if [[ "$1" == *"v"* ]];then var_trace; fi
+    if [[ "$1" == *"p"* ]];then pkg_trace; fi
 else
     echo "-- parse block ---"
     blk
     echo "-- parse variables ---"
     var
+    echo "-- parse pkg ---"
+    pkg
 fi
--- semantic/tests/run_tests.sh
+++ semantic/tests/run_tests.sh
@@ -9,23 +9,29 @@ LIKE_LIB=../../lib/pt
 SEMNUL="ptc -o3 -t3 -L $LIKE_LIB null.pt"
 SEMBLK="ptc -o3 -t3 -L $LIKE_LIB block.pt"
 SEMVAR="ptc -o3 -t3 -L $LIKE_LIB variables.pt"
+SEMPKG="ptc -o3 -t3 -L $LIKE_LIB pkg.pt"
 
 
 # nul_trace(){ ssltrace "$SEMNUL" $LIKE_LIB/semantic.def | egrep " *[\.o%]"; }
 nul_trace(){ ssltrace "$SEMNUL" $LIKE_LIB/semantic.def -e ; }
 blk_trace(){ ssltrace "$SEMBLK" $LIKE_LIB/semantic.def -e ; }
 var_trace(){ ssltrace "$SEMVAR" $LIKE_LIB/semantic.def -e ; }
+pkg_trace(){ ssltrace "$SEMPKG" $LIKE_LIB/semantic.def -e ; }
 
 blk(){ ptc -o3 -L $LIKE_LIB block.pt; }
 var(){ ptc -o3 -L $LIKE_LIB variables.pt; }
+pkg(){ ptc -o3 -L $LIKE_LIB pkg.pt; }
 
 if [ $# -ne 0 ]; then
     if [[ "$1" == *"n"* ]];then nul_trace; fi
     if [[ "$1" == *"b"* ]];then blk_trace; fi
     if [[ "$1" == *"v"* ]];then var_trace; fi
+    if [[ "$1" == *"p"* ]];then pkg_trace; fi
 else
     echo "-- semantic block ---"
     blk
     echo "-- semantic variables ---"
     var
+    echo "-- semantic pkg ---"
+    pkg
 fi
