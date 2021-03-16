fe35c5de24774d602f1908da5e1e52bb85da5d98
Benjamin Kitor <bkitor@gmail.com>
2021-03-16 00:12:11 -0400

    Update block and statement rules (#21)
    @rossj21 said this was good, please forward all complaints to him

--- semantic/tests/run_tests.sh
+++ semantic/tests/run_tests.sh
@@ -6,16 +6,26 @@ cd tests
 
 LIKE_LIB=../../lib/pt
 
-SEMBLK="ptc -o3 -t3 -L $LIKE_LIB blank.pt"
+SEMNUL="ptc -o3 -t3 -L $LIKE_LIB null.pt"
+SEMBLK="ptc -o3 -t3 -L $LIKE_LIB block.pt"
+SEMVAR="ptc -o3 -t3 -L $LIKE_LIB variables.pt"
 
-blk_trace(){ ssltrace "$SEMBLK" $LIKE_LIB/semantic.def -e; }
 
-blk(){ ptc -o3 -L $LIKE_LIB stringType.pt; }
+# nul_trace(){ ssltrace "$SEMNUL" $LIKE_LIB/semantic.def | egrep " *[\.o%]"; }
+nul_trace(){ ssltrace "$SEMNUL" $LIKE_LIB/semantic.def -e ; }
+blk_trace(){ ssltrace "$SEMBLK" $LIKE_LIB/semantic.def -e ; }
+var_trace(){ ssltrace "$SEMVAR" $LIKE_LIB/semantic.def -e ; }
+
+blk(){ ptc -o3 -L $LIKE_LIB block.pt; }
+var(){ ptc -o3 -L $LIKE_LIB variables.pt; }
 
 if [ $# -ne 0 ]; then
+    if [[ "$1" == *"n"* ]];then nul_trace; fi
     if [[ "$1" == *"b"* ]];then blk_trace; fi
-
+    if [[ "$1" == *"v"* ]];then var_trace; fi
 else
-    echo "-- test blk ---"
+    echo "-- semantic block ---"
     blk
+    echo "-- semantic variables ---"
+    var
 fi
