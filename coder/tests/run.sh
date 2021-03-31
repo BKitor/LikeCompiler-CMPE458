7c47bbeaa3402b2bcd3137499a2be5c1792a315c
bkitor <bkitor@gmail.com>
2021-03-31 12:23:40 -0400

    testing framework

---
+++ coder/tests/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd ..
+make 
+cd tests
+
+LIKE_LIB=../../lib/pt
+
+PT_TEST_FILES=( "null" "put" )
+
+like_run_test(){ # takes the name of a test (without .pt) as arg 1 
+    echo "*** compiling $f.pt ***"
+    ptc -L $LIKE_LIB $1.pt
+    echo "*** running $f.out ***"
+    ./$1.out
+    echo
+    rm $1.out
+}
+
+SEMNUL="ptc -S -L $LIKE_LIB null.pt"
+
+if [ $# -ne 0 ]; then
+    if [[ "$1" == *"n"* ]];then like_run_test null; fi
+    if [[ "$1" == *"w"* ]];then like_run_test put; fi
+else
+    for f in "${PT_TEST_FILES[@]}"; do
+        like_run_test $f
+    done
+fi
---
+++ coder/tests/run_coder.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd ..
+make 
+cd tests
+
+LIKE_LIB=../../lib/pt
+
+PT_TEST_FILES=( "null" "put" )
+
+like_build_asm(){
+    ptc -S -L $LIKE_LIB $1.pt
+    cat $1.s
+    rm $1.s
+}
+
+
+if [ $# -ne 0 ]; then
+    if [[ "$1" == *"n"* ]];then like_build_asm null; fi
+    if [[ "$1" == *"w"* ]];then like_build_asm put; fi
+else
+    echo "-- semantic null ---"
+    for f in "${PT_TEST_FILES[@]}"; do
+        like_build_asm $f
+    done
+fi
