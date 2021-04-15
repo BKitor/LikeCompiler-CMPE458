82fcdda6b79237e8452e5804f74c780f853a62f6
Eric Leask <16efl@linux2.caslab.queensu.ca>
2021-04-15 11:39:07 -0400

    choose statements

--- coder/tests/run.sh
+++ coder/tests/run.sh
@@ -6,7 +6,7 @@ cd tests
 
 LIKE_LIB=../../lib/pt
 
-PT_TEST_FILES=( "null" "put" )
+PT_TEST_FILES=( "null" "put" "choose" )
 
 like_run_test(){ # takes the name of a test (without .pt) as arg 1 
     echo "*** compiling $f.pt ***"
@@ -22,6 +22,7 @@ SEMNUL="ptc -S -L $LIKE_LIB null.pt"
 if [ $# -ne 0 ]; then
     if [[ "$1" == *"n"* ]];then like_run_test null; fi
     if [[ "$1" == *"w"* ]];then like_run_test put; fi
+    if [[ "$1" == *"c"* ]];then like_run_test choose; fi
 else
     for f in "${PT_TEST_FILES[@]}"; do
         like_run_test $f
