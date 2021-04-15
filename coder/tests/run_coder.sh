06daf05db6f7b48febc837b2ac6853f330875869
bkitor <bkitor@gmail.com>
2021-04-15 15:49:09 -0400

    Initial values for Ints

--- coder/tests/run_coder.sh
+++ coder/tests/run_coder.sh
@@ -6,7 +6,7 @@ cd tests
 
 LIKE_LIB=../../lib/pt
 
-PT_TEST_FILES=( "null" "put" )
+PT_TEST_FILES=( "null" "put" "init_val")
 
 like_build_asm(){
     ptc -S -L $LIKE_LIB $1.pt
@@ -18,6 +18,7 @@ like_build_asm(){
 if [ $# -ne 0 ]; then
     if [[ "$1" == *"n"* ]];then like_build_asm null; fi
     if [[ "$1" == *"w"* ]];then like_build_asm put; fi
+    if [[ "$1" == *"i"* ]];then like_build_asm init_val; fi
 else
     echo "-- semantic null ---"
     for f in "${PT_TEST_FILES[@]}"; do
