38f4a866832c5ddc63fdb2f39b53e358b6f70eb8
Eric Leask <16efl@linux2.caslab.queensu.ca>
2021-04-19 13:56:55 -0400

    testing for arrays

--- coder/tests/run.sh
+++ coder/tests/run.sh
@@ -8,7 +8,7 @@ LIKE_LIB=../../lib/pt
 
 
 
-PT_TEST_FILES=( "null" "put" "init_val" "choose" "strings" )
+PT_TEST_FILES=( "null" "put" "init_val" "choose" "strings" "string_arrays" )
 
 like_run_test(){ # takes the name of a test (without .pt) as arg 1 
     echo "*** compiling $f.pt ***"
@@ -27,6 +27,7 @@ if [ $# -ne 0 ]; then
     if [[ "$1" == *"c"* ]];then like_run_test choose; fi
     if [[ "$1" == *"i"* ]];then like_run_test init_val; fi
     if [[ "$1" == *"s"* ]];then like_run_test strings; fi
+    if [[ "$1" == *"a"* ]];then like_run_test string_arrays; fi
 
 else
     for f in "${PT_TEST_FILES[@]}"; do
